b3b95ac1f9672d64b8189fccc8587ce66bbdd583
ankan-mazumdar <69012134+ankan-mazumdar@users.noreply.github.com>
2022-10-17 20:46:03 +0530

    Add files via upload

--- stream_app.py
+++ stream_app.py
@@ -114,7 +114,15 @@ def main():
             #print('retrain_model=====',retrain_model)
             #else:
                     #print('no model.h5 for retraining found') 
-            retrain_model = tf.keras.models.load_model('retrained_X_test100_79_model.h5')        
+                    
+            import subprocess
+            if not os.path.isfile('model.h5'):
+                subprocess.run(['curl --output model.h5 "https://media.githubusercontent.com/media/ankan-mazumdar/Active-Learning2/main/retrained_X_test100_79_model.h5"'], shell=True)
+                
+            #Replace model = tf.keras.models.load_model('sep_5.h5', compile=False) with:
+            #model = tf.keras.models.load_model('model.h5', compile=False)
+            #Don’t delete or rename the sep_5.h5 file from your repo, as we’re using its url to download your model in Step 1        
+            retrain_model = tf.keras.models.load_model('model.h5')        
             es_callbacks=[tf.keras.callbacks.EarlyStopping(patience=0, verbose=1)]
             opt = tf.keras.optimizers.Adam(1e-3)
             # compile the model
