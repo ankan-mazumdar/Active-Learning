497e3af31b8052b46e1d9c271257e09c0cee9bfe
ankan-mazumdar <69012134+ankan-mazumdar@users.noreply.github.com>
2022-10-18 19:01:23 +0530

    Add files via upload

--- stream_app.py
+++ stream_app.py
@@ -125,7 +125,7 @@ def main():
             #model = tf.keras.models.load_model('model.h5', compile=False)
             #Don’t delete or rename the sep_5.h5 file from your repo, as we’re using its url to download your model in Step 1        
             retrain_model = tf.keras.models.load_model('model.h5') 
-            st.write(model.h5)
+            print(retrain_model)
             es_callbacks=[tf.keras.callbacks.EarlyStopping(patience=6, verbose=1)]
             opt = tf.keras.optimizers.Adam(1e-3)
             # compile the model
